cfe3607dd8e337d02eb0e2ae87b290edd1b0c222
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-10-31 13:35:14 +0600

    chat-css

---
+++ src/db.js
@@ -0,0 +1,19 @@
+import firebase  from 'firebase/app';
+import "firebase/database";
+
+const config = {
+    apiKey: "AIzaSyACcrUk90iXuO9RMG94yoJXQOYncDyTf-Y",
+  authDomain: "firevuechat-1e424.firebaseapp.com",
+  projectId: "firevuechat-1e424",
+  storageBucket: "firevuechat-1e424.appspot.com",
+  messagingSenderId: "298223085836",
+  appId: "1:298223085836:web:742c6b4f33ca53e2b78129"
+}
+const db = firebase.initializeApp(config);
+export default db;
+
+
+
+
+
+
