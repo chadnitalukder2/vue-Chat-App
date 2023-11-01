6623b3d7e1bf86622756d2d43fc2b3e81c908a02
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-11-01 23:00:38 +0600

    chat-com

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,50 @@
 <template>
-  
-  <div class="view login">
-    <form class="login-form">
-      <div class="form-inner">
-        <h1>Login to FireChat</h1>
-        <label for="username">Username</label>
-        <input
-          type="text"
-          v-model="inputUsername"
-          placeholder="Please enter your username.... "
-        />
-        <input type="submit" value="Login" />
-      </div>
-    </form>
-
-    <div class="view chat">
-      
+  <div class="app">
+    <div
+      class="view login"
+      v-if="state.username === '' || state.username === null"
+    >
+      <form class="login-form" @submit.prevent="Login">
+        <div class="form-inner">
+          <h1>Login to FireChat</h1>
+          <label for="username">Username</label>
+          <input
+            type="text"
+            v-model="inputUsername"
+            placeholder="Please enter your username.... "
+          />
+          <input type="submit" value="Login" />
+        </div>
+      </form>
+    </div>
+    <div class="view chat" v-else>
+      <header>
+        <button class="logout" @click="Logout">Logout</button>
+        <h1>Welcome, {{ state.username }}</h1>
+      </header>
+      <section class="chat-box">
+      <div
+        v-for="message in state.messages"
+          :key="message.key"
+          :class="(message.username == state.username ? 'message current-user' : 'message')"> 
+        <div class="message-inner">
+          <div class="username">{{ message.username }}</div>
+          <div class="content">{{ message.content }}</div>
+        </div>
+        </div>
+      </section>
+      <footer>
+        <form @submit.prevent="SendMessage">
+          <input
+            type="text"
+            v-model="inputMessage"
+            placeholder="Write a message"
+          />
+          <input type="submit" value="Send" />
+        </form>
+      </footer>
     </div>
   </div>
-  
 </template>
 
 <script>
@@ -27,17 +53,58 @@ import db from "./db";
 export default {
   setup() {
     const inputUsername = ref("");
+    const inputMessage = ref("");
+
     const state = reactive({
       username: "",
-    })
-    const Login = () =>{
-      if(inputUsername.value != "" || inputUsername.value != null){
-        state
+      messages: [],
+    });
+    const Login = () => {
+      if (inputUsername.value != "" || inputUsername.value != null) {
+        state.username = inputUsername.value;
+        inputUsername.value = "";
       }
     }
+    const Logout = () =>{
+      state.username = "";
+    }
+
+    const SendMessage = () => {
+      const messageRef = db.database().ref("messages");
+
+      if (inputMessage.value === "" || inputMessage.value === null) {
+        return;
+      }
+      const message = {
+        username: state.username,
+        content: inputMessage.value,
+      };
+      messageRef.push(message);
+      inputMessage.value = "";
+    };
+    onMounted(() => {
+      const messageRef = db.database().ref("messages");
+      messageRef.on("value", (snapshot) => {
+        const data = snapshot.val();
+        let messages = [];
+
+        Object.keys(data).forEach((key) => {
+          messages.push({
+            id: key,
+            username: data[key].username,
+            content: data[key].content,
+          });
+        });
+        state.messages = messages;
+      });
+    });
     return {
-      db,
       inputUsername,
+      Login,
+      state,
+      inputMessage,
+      SendMessage,
+      Logout,
     };
   },
 };
